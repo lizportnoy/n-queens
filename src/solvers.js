d24000ae15e0039970be9a26f7e4a0444128a9e9
Liz Portnoy <lizport10@gmail.com>
2014-11-05 23:41:18 -0800

    solved Rooks (slowly);

--- src/solvers.js
+++ src/solvers.js
@@ -36,50 +36,23 @@ window.countNRooksSolutions = function(n) {
   var solutionCount = 0;
   var board = new Board({'n':n});
   var boardSize = n;
-  // for (var i = 0; i < board.get(0).length; i++) {
-  //   board.togglePiece(i,0);
-  // }
-  // debugger;
+
   var row = 0;
-
-  // var rooksRecurse = function(n, row){
-  //     for (var i = 0; i < board.get(0).length; i++) {
-  //       // iF add 1 to i here, should untoggle the prior one
-  //       board.togglePiece(row,i);
-  //       debugger;
-
-  //       if (i > 0 && board.get(row, i-1) === 1) {
-  //         board.togglePiece(row, i -1);
-  //       }
-  //       debugger;
-
-  //       if(board.hasAnyRooksConflicts()) {
-  //         board.togglePiece(row, i);
-
-  //       } else {
-  //         n--;
-
-  //         if (n> 0) {
-  //           row++;
-  //         }
-  //         else if (n === 0){
-  //           solutionCount++;
-  //           board.togglePiece(row, i);
-  //         } else {
-  //         rooksRecurse(n,row);
-  //       }
-  //         }
-  //       }
-  //     };
 
   var rooksRecurse = function (n, row) {
     for (var i = 0; i < boardSize; i++) {
       board.togglePiece(row, i);
+      for (var m = row+1; m<boardSize; m++) {
+        for ( var n = 0; n < boardSize ; n++) {
+          if (board.get(m)[n] === 1) {
+            board.togglePiece(m, n);
+          }
+        }
+      }
 
       if (i > 0 && board.get(row)[i-1] === 1) {
         board.togglePiece(row, i - 1);
       }
-      debugger;
 
       if(board.hasAnyRooksConflicts()) {
         board.togglePiece(row, i);
@@ -89,21 +62,12 @@ window.countNRooksSolutions = function(n) {
           rooksRecurse(n - 1, row + 1);
         } else {
           solutionCount++;
-          board.togglePiece(row, i);
-          for (var j = 2; j < row; j++) {
-            for (var k = 0; k < boardSize; k++) {
-              if(board.get(j)[k] === 1) {
-                board.togglePiece(j, k);
-              }
-            }
-          }
         }
       }
     }
   };
 
 
-  //   // add to count if you've found a viable solution
   rooksRecurse(n,row);
 
 
