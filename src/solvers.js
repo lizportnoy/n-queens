b495fb89b2f293ff3d9d5e64690f85b783e34af0
Liz Portnoy <lizport10@gmail.com>
2014-11-06 19:05:59 -0800

    Made optimizations to code

--- src/solvers.js
+++ src/solvers.js
@@ -12,190 +12,73 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
+var findSolution = function(n, row, board, verifier, callBack) {
+  if (n === row) {
+    return callBack(board.rows());
+  };
+  for (var i = 0; i < n; i++) {
+    board.togglePiece(row, i);
+
+    if (!board[verifier]()) {
+      if (findSolution(n, row+1, board, verifier, callBack)) {
+        return true};
+    }
+
+    board.togglePiece(row,i);
+
+  }
+
+};
 
 window.findNRooksSolution = function(n) {
-  var solution = []; //fixme
-  // if loop through board of n to the power of n
-  var board = new Board({'n':n});
-  for (var i = 0; i<n; i++) {
-    board.togglePiece(i,i);
-  }
+  var solution;
+  var board = new Board({n:n});
+  var callBack = function (result) {solution = result; return true;};
 
-  for (var i = 0; i < n; i++) {
-    solution.push(board.get(i));
-  }
+  findSolution(n, 0, board, 'hasAnyRooksConflicts', callBack);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution 
 };
 
 
-
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  if (n === 0) {
-    return 1;
-  }
-  // debugger;
   var solutionCount = 0;
-  var board = new Board({'n':n});
-  var boardSize = n;
+  var board = new Board({n:n});
 
-  var rooksRecurse = function (n, row) {
-    //Establish base row and reset following rows 0
-    for (var i = 0; i < boardSize; i++) {
-      //Base row and toggle the row
-      board.togglePiece(row, i);
-      //Iterating through following rows
-      for (var rowBelow = row+1; rowBelow <boardSize; rowBelow++) {
-        //Iterate through columns
-        for ( var colInd = 0; colInd < boardSize ; colInd++) {
-          //There is 1 in specified row, toggle back to 0
-          if (board.get(rowBelow)[colInd] === 1) {
-            board.togglePiece(rowBelow, colInd);
-          }
-        }
-      }
+  var callBack = function() {
+    solutionCount++;
+  }
 
-      //Toggle piece for previous columns on the same row
-      if (i > 0 && board.get(row)[i-1] === 1) {
-        board.togglePiece(row, i - 1);
-      }
-
-      if(board.hasAnyRooksConflicts()) {
-        board.togglePiece(row, i);
-      } else {
-        // if there wasn't a conflict, you'll want to keep going with problem
-        if (row+1 < boardSize) {
-          rooksRecurse(n - 1, row + 1);
-        } else {
-          solutionCount++;
-        }
-      }
-    }
-  };
-
-  rooksRecurse(n,0);
-
+  findSolution(n, 0, board, 'hasAnyRooksConflicts', callBack);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
+
 };
 
-
-
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
+  var solution;
+  var board = new Board({n:n});
+  var callBack = function (result) {solution = result; return true;};
 
+  findSolution(n, 0, board, 'hasAnyQueensConflicts', callBack);
 
-
-    var board = new Board({'n':n});
-    var boardSize = n;
-    var result = [];
-    var found = false;
-
-    var queensRecurse = function (n, row) {
-
-      //Establish base row and reset following rows 0
-      for (var i = 0; i < boardSize; i++) {
-        if (found) {
-          return;
-        }
-        //Base row and toggle the row
-        board.togglePiece(row, i);
-        debugger;
-
-        //Iterating through following rows
-        for (var rowBelow = row+1; rowBelow <boardSize; rowBelow++) {
-          //Iterate through columns
-          for ( var colInd = 0; colInd < boardSize ; colInd++) {
-            //There is 1 in specified row, toggle back to 0
-            if (board.get(rowBelow)[colInd] === 1) {
-              board.togglePiece(rowBelow, colInd);
-            }
-          }
-        }
-
-        //Toggle piece for previous columns on the same row
-        if (i > 0 && board.get(row)[i-1] === 1) {
-          board.togglePiece(row, i - 1);
-        }
-
-        if(board.hasAnyQueensConflicts()) {
-          board.togglePiece(row, i);
-        } else {
-          // if there wasn't a conflict, you'll want to keep going with problem
-          if (row+1 < boardSize) {
-            queensRecurse(n - 1, row + 1);
-          } else {
-            debugger;
-            if(!found) {
-            for (var k = 0; k < boardSize; k++) {
-              result.push(board.get(k).slice());
-            }
-            found = true;
-            }
-          }
-        }
-      }
-    };
-
-    queensRecurse(n,0);
-
-    if (!found) {
-      result = {n: n}
-    }
-
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(result));
-  return result;
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  return !solution ? {n:n} : solution;
 };
 
 
-// return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
+  var solutionCount = 0;
+  var board = new Board({n:n});
 
-    if (n === 0) {
-      return 1;
-    }
-    // debugger;
-    var solutionCount = 0;
-    var board = new Board({'n':n});
-    var boardSize = n;
+  var callBack = function() {
+    solutionCount++;
+  }
 
-    var queensRecurse = function (n, row) {
-      //Establish base row and reset following rows 0
-      for (var i = 0; i < boardSize; i++) {
-        //Base row and toggle the row
-        board.togglePiece(row, i);
-        //Iterating through following rows
-        for (var rowBelow = row+1; rowBelow <boardSize; rowBelow++) {
-          //Iterate through columns
-          for ( var colInd = 0; colInd < boardSize ; colInd++) {
-            //There is 1 in specified row, toggle back to 0
-            if (board.get(rowBelow)[colInd] === 1) {
-              board.togglePiece(rowBelow, colInd);
-            }
-          }
-        }
+  findSolution(n, 0, board, 'hasAnyQueensConflicts', callBack);
+  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  return solutionCount;
 
-        //Toggle piece for previous columns on the same row
-        if (i > 0 && board.get(row)[i-1] === 1) {
-          board.togglePiece(row, i - 1);
-        }
-
-        if(board.hasAnyQueensConflicts()) {
-          board.togglePiece(row, i);
-        } else {
-          // if there wasn't a conflict, you'll want to keep going with problem
-          if (row+1 < boardSize) {
-            queensRecurse(n - 1, row + 1);
-          } else {
-            solutionCount++;
-          }
-        }
-      }
-    };
-
-    queensRecurse(n,0);
-
-    console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-    return solutionCount;
 };
+
+
